b9a26d9df508f941380d3ddffacc7540b506ee3a
destiny3912 <destiny3910@gmail.com>
2025-03-03 21:56:22 +0900

    project init

---
+++ src/app/api/route.tsx
@@ -0,0 +1,9 @@
+export async function GET() {
+    const db = await database.connect({
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    }),
+
+}
